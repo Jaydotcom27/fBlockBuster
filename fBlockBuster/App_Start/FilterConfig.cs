33560e1065b1d7c376b0fdd374ca1714c96221b6
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-03-29 23:45:08 -0400

    Agregar archivos de proyecto.

---
+++ fBlockBuster/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace fBlockBuster
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
