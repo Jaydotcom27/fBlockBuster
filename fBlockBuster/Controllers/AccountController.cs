359fe3edf0173a60ab7ca7d4bb7a9d86d1ce051a
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2020-03-31 23:28:27 -0400

    Login, Register / Authentication Modules

--- fBlockBuster/Controllers/AccountController.cs
+++ fBlockBuster/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using fBlockBuster.Models;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
 namespace fBlockBuster.Controllers
 {
@@ -14,35 +15,90 @@ namespace fBlockBuster.Controllers
         SqlConnection con = new SqlConnection();
         SqlCommand com = new SqlCommand();
         SqlDataReader dr;
+
         // GET: Account
         [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
+
         void connectionString()
         {
             con.ConnectionString = "data source =JAYDESK; database=BlockBusterDB; integrated security = SSPI;";
 
         }
+
         [HttpPost]
+
         public ActionResult Verify(Account acc)
         {
-            connectionString();
-            con.Open();
-            com.Connection = con;
-            com.CommandText = "select * from tblUsuario where NombreUsuario='" +acc.Name+"' and PasswordUsuario='"+acc.Password+"'" ;
-            dr = com.ExecuteReader();
-            if (dr.Read())
+            string ConnectionString = "Integrated Security = True; " +
+           "Initial Catalog= BlockBusterDB; " + " Data source = JAYDESK; ";
+            string SQL = "select * from tblUsuario where NombreUsuario='" + acc.Name + "' and PasswordUsuario='" + acc.Password + "'";
+
+            SqlConnection conn = new SqlConnection(ConnectionString);
+
+            // Create a command object
+            SqlCommand cmd = new SqlCommand(SQL, conn);
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
             {
-                con.Close();
-                return View("Create");
+                bool isAdmin = false;
+                int Tipo = Convert.ToInt32(reader.GetSqlInt32(reader.GetOrdinal("idTipo")).Value);
+                if (Tipo == 10)
+                {
+                    isAdmin = true;
+                }
+
+                if (isAdmin)
+                {
+                    con.Close();
+                    ViewBag.Tipo = Tipo;
+                    return View($"~/Views/Home/Index.cshtml");
+                }
+                else
+                {
+                   con.Close();
+                   return View($"~/Views/Home/Index.cshtml");
+                   }
             }
             else
             {
                 con.Close();
                 return View("Error");
+
             }
+
+        }
+
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult aCreate(Account acc)
+        {
+            connectionString();
+            com.CommandText = "INSERT into tblUsuario (NombreUsuario, PasswordUsuario, idTipo) VALUES ('" + acc.Name + "', '" + acc.Password + "', '0')";
+            com.Connection = con;
+            try
+            {
+                con.Open();
+                com.ExecuteNonQuery();
+                con.Close();
+                return View($"~/Views/Home/Index.cshtml");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
         }
     }
 }
--- fBlockBuster/Models/Account.cs
+++ fBlockBuster/Models/Account.cs
@@ -9,6 +9,6 @@ namespace fBlockBuster.Models
     {
         public string Name { get; set; }
         public string Password { get; set; }
-
+        public string Type { get; set; }
     }
 }
